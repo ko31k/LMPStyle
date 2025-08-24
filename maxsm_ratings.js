86c315607908da1136e609a9e4802e6aec2e4936
ko31k <kozak.bohdan@gmail.com>
2025-08-24 17:22:53 +0300

    maxsm_ratings.js

--- maxsm_ratings.js
+++ maxsm_ratings.js
@@ -6,8 +6,8 @@
 Можно использовать одиночный ключ или массив ключей, после получения API ключей передайте их как массивы через:
     window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.OMDB_API_KEYS
 Или просто введите ниже в коде плагина:
-    var OMDB_API_KEYS = (window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.OMDB_API_KEYS) || ['66427d3d']; // api ключи массивом
-    var KP_API_KEYS   = (window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.KP_API_KEYS)   || ['407']; // api ключи массивом
+    var OMDB_API_KEYS = (window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.OMDB_API_KEYS) || ['406']; // api ключи массивом
+    var KP_API_KEYS   = (window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.KP_API_KEYS)   || ['3941c891-2339-46d3-be72-f924b3b594a6']; // api ключи массивом
 
 Для получения данных о качестве используется jacred парсер, по умолчанию плагин настроен на получение адреса и ключа вашего введеного jacred,
 вы можете изменить это в переменных:
@@ -2427,5 +2427,6 @@
     if (!window.maxsmRatingsPlugin) startPlugin();
 
 })();
+
 
 

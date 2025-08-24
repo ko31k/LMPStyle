8b0b68fb2d15f21457f0704ca9208194d85f8bbb
ko31k <kozak.bohdan@gmail.com>
2025-08-24 15:16:49 +0300

    cache.js

--- cache.js
+++ cache.js
@@ -76,16 +76,6 @@
         }
 
         // Додавання кнопки очищення кешу
-        $('.head__actions').append(`
-            <div id="${clearBtnId}" class="head__action selector m-clear-cache">
-                <svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 3.1l1.4 2.2-1.6 1.1 1.3 0.3 2.8 0.6 0.6-2.7 0.4-1.4-1.8 1.1-2-3.3h-2.2l-2.6 4.3 1.7 1z"/>
-                    <path d="M16 12l-2.7-4.3-1.7 1 2 3.3h-2.6v-2l-3 3 3 3v-2h3.7z"/>
-                    <path d="M2.4 12v0l1.4-2.3 1.7 1.1-0.9-4.2-2.8 0.7-1.3 0.3 1.6 1-2.1 3.4 1.3 2h5.7v-2z"/>
-                </svg>
-            </div>
-        `);
-
         // Оригінальний обробник для TV (повертаємо стару логіку)
         $('#' + clearBtnId).on('hover:enter hover:click hover:touch', function() {
             try {

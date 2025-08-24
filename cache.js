7008c11fd7735ed46fd5fd8f13ceef16d73f6151
ko31k <kozak.bohdan@gmail.com>
2025-08-24 16:06:33 +0300

    cache.js

--- cache.js
+++ cache.js
@@ -99,27 +99,53 @@
                         setTimeout(() => location.reload(), 300);
                     }, 800);
                 } else {
-                    setTimeout(() => {
-                        let removed = 0;
-                        const keysToRemove = [];
-                        for (let i = 0; i < localStorage.length; i++) {
-                            const key = localStorage.key(i);
-                            if (key.startsWith('card_') || key.startsWith('full_card_') || 
-                                key.startsWith('lite_card_') || key.startsWith('viewed_card_') || 
-                                key.startsWith('viewed_continue_') || key.startsWith('parser_') || 
-                                key.startsWith('cub_') || key.startsWith('start_time_') || 
-                                key.startsWith('cache_')) {
-                                keysToRemove.push(key);
+                    if (confirm('Очистить кеш? Данные парсера будут сохранены.\n\nНажмите OK для очистки без данных парсера\nИли Отмена для полной очистки')) {
+                        // Очистка БЕЗ данных парсера
+                        setTimeout(() => {
+                            let removed = 0;
+                            const keysToRemove = [];
+                            for (let i = 0; i < localStorage.length; i++) {
+                                const key = localStorage.key(i);
+                                if ((key.startsWith('card_') || key.startsWith('full_card_') || 
+                                     key.startsWith('lite_card_') || key.startsWith('viewed_card_') || 
+                                     key.startsWith('viewed_continue_') || key.startsWith('cub_') || 
+                                     key.startsWith('start_time_') || key.startsWith('cache_')) &&
+                                     !key.startsWith('parser_')) {
+                                    keysToRemove.push(key);
+                                }
                             }
-                        }
-                        keysToRemove.forEach(key => {
-                            localStorage.removeItem(key);
-                            removed++;
-                        });
-                        alert(`🗑 Локальний кеш очищено: ${removed} ключів`);
-                        $(this).removeClass('loading');
-                        setTimeout(() => location.reload(), 300);
-                    }, 800);
+                            keysToRemove.forEach(key => {
+                                localStorage.removeItem(key);
+                                removed++;
+                            });
+                            alert(` Локальний кеш очищено: ${removed} ключів (данные парсера сохранены)`);
+                            $(this).removeClass('loading');
+                            setTimeout(() => location.reload(), 300);
+                        }, 800);
+                    } else {
+                        // Полная очистка ВКЛЮЧАЯ данные парсера
+                        setTimeout(() => {
+                            let removed = 0;
+                            const keysToRemove = [];
+                            for (let i = 0; i < localStorage.length; i++) {
+                                const key = localStorage.key(i);
+                                if (key.startsWith('card_') || key.startsWith('full_card_') || 
+                                    key.startsWith('lite_card_') || key.startsWith('viewed_card_') || 
+                                    key.startsWith('viewed_continue_') || key.startsWith('parser_') || 
+                                    key.startsWith('cub_') || key.startsWith('start_time_') || 
+                                    key.startsWith('cache_')) {
+                                    keysToRemove.push(key);
+                                }
+                            }
+                            keysToRemove.forEach(key => {
+                                localStorage.removeItem(key);
+                                removed++;
+                            });
+                            alert(` Полная очистка кеша: ${removed} ключів (включая данные парсера)`);
+                            $(this).removeClass('loading');
+                            setTimeout(() => location.reload(), 300);
+                        }, 800);
+                    }
                 }
             } catch (e) {
                 console.error('Помилка очищення кешу:', e);
@@ -131,37 +157,4 @@
         function updateButtons() {
             $('.full-start__button.view--torrent svg').replaceWith(`
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-                    <path d="M25,2C12.317,2,2,12.317,2,25s10.317,23,23,23s23-10.317,23-23S37.683,2,25,2zM40.5,30.963c-3.1,0-4.9-2.4-4.9-2.4S34.1,35,27,35c-1.4,0-3.6-0.837-3.6-0.837l4.17,9.643C26.727,43.92,25.874,44,25,44c-2.157,0-4.222-0.377-6.155-1.039L9.237,16.851c0,0-0.7-1.2,0.4-1.5c1.1-0.3,5.4-1.2,5.4-1.2s1.475-0.494,1.8,0.5c0.5,1.3,4.063,11.112,4.063,11.112S22.6,29,27.4,29c4.7,0,5.9-3.437,5.7-3.937c-1.2-3-4.993-11.862-4.993-11.862s-0.6-1.1,0.8-1.4c1.4-0.3,3.8-0.7,3.8-0.7s1.105-0.163,1.6,0.8c0.738,1.437,5.193,11.262,5.193,11.262s1.1,2.9,3.3,2.9c0.464,0,0.834-0.046,1.152-0.104c-0.082,1.635-0.348,3.221-0.817,4.722C42.541,30.867,41.756,30.963,40.5,30.963z"/>
-                </svg>
-            `);
-
-            $('.full-start__button.view--online svg').replaceWith(`
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                    <path d="M20.331 14.644l-13.794-13.831 17.55 10.075zM2.938 0c-0.813 0.425-1.356 1.2-1.356 2.206v27.581c0 1.006 0.544 1.781 1.356 2.206l16.038-16zM29.512 14.1l-3.681-2.131-4.106 4.031 4.106 4.031 3.756-2.131c1.125-0.893 1.125-2.906-0.075-3.8zM6.538 31.188l17.55-10.075-3.756-3.756z"/>
-                </svg>
-            `);
-
-            $('.full-start__button.view--trailer svg').replaceWith(`
-                <svg height="70" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M71.2555 2.08955C74.6975 3.2397 77.4083 6.62804 78.3283 10.9306C80 18.7291 80 35 80 35C80 35 80 51.2709 78.3283 59.0694C77.4083 63.372 74.6975 66.7603 71.2555 67.9104C65.0167 70 40 70 40 70C40 70 14.9833 70 8.74453 67.9104C5.3025 66.7603 2.59172 63.372 1.67172 59.0694C0 51.2709 0 35 0 35C0 35 0 18.7291 1.67172 10.9306C2.59172 6.62804 5.3025 3.2395 8.74453 2.08955C14.9833 0 40 0 40 0C40 0 65.0167 0 71.2555 2.08955ZM55.5909 35.0004L29.9773 49.5714V20.4286L55.5909 35.0004Z"/>
-                </svg>
-            `);
-        }
-
-        // Ініціалізація
-        updateButtons();
-        if (Lampa && Lampa.Listener) {
-            Lampa.Listener.follow('full', updateButtons);
-        }
-
-        // Реєстрація плагіна
-        window.plugin && window.plugin('clear_cache_plugin', {
-            type: 'component',
-            name: 'Оптилізовані кнопки + очистка кешу',
-            version: '2.3.1',
-            author: 'Oleksandr',
-            description: 'Фікс для TV + стандартні анімації + синя кнопка Стрічка'
-        });
-
-    }, 1000);
-})();
+                    <path d="M25,2C12.317,2,2,12.317,2,25s10.317,23,23,23s23-10.317,23-23S37.683,2,25,2zM40.5,30.963c-3.1,0-4.9-2.4-4.9-2.4S34.1,35,27,35c-1.4,0-3.6-0.837-3.6-0.837l4.17,9.643C26.727,43.92,25.874,44,25,44c-2.157,0-4.222-0.377-6.155-1.039L9.237,16.851c0,0-0.7-1.2,0.4-1.5c1.1-0.3,5.4-1.2,5.4-1.2s1.475-0.494

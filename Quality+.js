518fdcc0157b4c948489ee881fc79231c3df5a1d
ko31k <kozak.bohdan@gmail.com>
2025-08-28 22:31:52 +0300

    Quality+.js

--- Quality+.js
+++ Quality+.js
@@ -174,7 +174,7 @@ var styleLQE = "<style id=\"lampa_quality_styles\">" +
     "}" +
     ".card__quality {" +
     " position: absolute; " +
-    " bottom: 3.0em; " +
+    " bottom: 2.2em; " +
     " left: 0; " +
     " background-color: " + (LQE_CONFIG.LIST_CARD_LABEL_BACKGROUND_TRANSPARENT ? "transparent" : LQE_CONFIG.LIST_CARD_LABEL_BACKGROUND_COLOR) + " !important;" +
     " z-index: 10;" +

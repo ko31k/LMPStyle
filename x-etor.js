6c6028f089e7ff9ef26525f7ebb1bf7f22f438f2
ko31k <kozak.bohdan@gmail.com>
2025-09-14 20:19:16 +0300

    Create x-etor.js

---
+++ x-etor.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+
+	// Не потрібно переписувати весь цей код, не робіть так!
+	//for Lampa.
+	window.lampa_settings.torrents_use = true;
+	window.lampa_settings.demo = false;
+	window.lampa_settings.read_only = false;
+
+})();

93049675026b355d784bdca9d1b7dc1eadd4887c
ko31k <kozak.bohdan@gmail.com>
2025-10-25 00:11:12 +0300

    Update Quality+Mod.js

--- Quality+Mod.js
+++ Quality+Mod.js
@@ -82,7 +82,18 @@
         		quality_code: 1080, 
         		full_label: '1080 ВDRemux', //✅ Повна мітка
         		simple_label: 'FHD'  	 //✅ Спрощена мітка
+    		},
+    		'604079': { 
+        		quality_code: 2160, 
+        		full_label: '4K WEB-DL', //✅ Повна мітка
+        		simple_label: '4K'  	 //✅ Спрощена мітка
+    		},
+	    	'1267905': { 
+        		quality_code: 2160, 
+        		full_label: '4K WEB-DL', //✅ Повна мітка
+        		simple_label: '4K'  	 //✅ Спрощена мітка
     		}
+
 			/*'Тут ID фільму': { 
         		quality_code: 1080, 
         		full_label: '1080p WEB-DLRip',  //✅ Повна мітка

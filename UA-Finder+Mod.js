4d37f52113ee6b6232fdd2e81dd32b4b4b6ff33d
ko31k <kozak.bohdan@gmail.com>
2026-01-30 20:58:21 +0200

    Update UA-Finder+Mod.js

--- UA-Finder+Mod.js
+++ UA-Finder+Mod.js
@@ -48,13 +48,13 @@
         JACRED_PROTOCOL: 'http://', // Протокол для API JacRed.
         JACRED_URL: 'redapi.cfhttp.top', // Домен API JacRed (redapi.cfhttp.top або jacred.xyz)
         PROXY_LIST: [ // Список проксі-серверів для обходу CORS-обмежень.
-            'https://my-finder.kozak-bohdan.workers.dev/?key=lqe_2026_x9A3fQ7P2KJmLwD8N4s0Z&url=',
+            'https://myfinder.kozak-bohdan.workers.dev/?key=lqe_2026_x9A3fQ7P2KJmLwD8N4s0Z&url=',
             //'https://api.allorigins.win/raw?url=',
             //'https://cors.bwa.workers.dev/'
             
         ],
-        PROXY_TIMEOUT_MS: 2500, // Максимальний час очікування відповіді від одного проксі (2.5 секунди).
-        MAX_PARALLEL_REQUESTS: 10, // Максимальна кількість одночасних запитів до API.
+        PROXY_TIMEOUT_MS: 3000, // Максимальний час очікування відповіді від одного проксі (2.5 секунди).
+        MAX_PARALLEL_REQUESTS: 12, // Максимальна кількість одночасних запитів до API.
         MAX_RETRY_ATTEMPTS: 2, // (Зараз не використовується, але зарезервовано).
 
         // Налаштування функціоналу
